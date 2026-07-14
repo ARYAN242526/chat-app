348a4d798e84e98bba05b8091e9f88e1d33ebb0e
ARYAN242526 <roy2503aryan@gmail.com>
2026-07-14 23:36:43 +0530

    feat : add axios instance and implement auth store with socket integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,27 @@ import ChatPage from './pages/ChatPage';
 import AuthPage from './pages/AuthPage';
 import {useAuth} from '@clerk/react';
 import PageLoader from './components/PageLoader';
+import { useAuthStore } from './store/useAuthStore';
+import { useEffect } from 'react';
+
+import {Toaster} from 'react-hot-toast';
 
 function App() {
   const {isSignedIn, isLoaded} = useAuth();
 
-  if(!isLoaded) return <PageLoader />;
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if(!isLoaded) return;
+
+    if(isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  if(!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
+
 
   return (
     <ThemeProvider>
@@ -20,8 +36,8 @@ function App() {
           path='/auth'
           element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />} 
         />
-
       </Routes>
+      <Toaster />
       </WallpaperProvider>
     </ThemeProvider>
   )
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,6 +1,11 @@
+import toast from "react-hot-toast"
+
 function ChatPage() {
   return (
-    <div>ChatPage</div>
+    <div>
+      ChatPage
+      <button onClick={() => toast.success("you clicked")}>Click me</button>
+    </div>
   )
 }
 
